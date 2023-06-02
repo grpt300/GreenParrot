f835306d8f5212457985e7904e2d86cf341e4a5e
venkatesansubramanian80 <venkatesangit80@outlook.com>
2023-06-02 17:40:18 -0500

    GP-9 drilling down

--- POC/earnings_data_analyzer.py
+++ POC/earnings_data_analyzer.py
@@ -18,7 +18,18 @@ if __name__ == "__main__":
     df_1['reported_actuals_diff'] = df_1['reported_Quarterly_EPS'] - df_1['estimated_Quarterly_EPS']
     # filter df_1 to have reported_actuals_diff greater than 0
     df_1 = df_1[df_1['reported_actuals_diff'] > 0]
-    df_1.to_csv("influxdata_2023-06-02T18_44_38Z_3.csv", index=False)
+    #df_1.to_csv("influxdata_2023-06-02T18_44_38Z_3.csv", index=False)
     # Loop throguh grouping based on symbol and for each print the symbol and the count of rows
+    all_groups = []
     for symbol, group in df_1.groupby('symbol'):
         print(symbol, len(group))
+        # sort group by time column
+        group = group.sort_values(by=['time'])
+        group['moving_estimate'] = group['estimated_Quarterly_EPS'].diff()
+        group['moving_reported'] = group['reported_Quarterly_EPS'].diff()
+        # Convert group dataframe to dictionary
+        group_dict = group.to_dict('records')
+        # put all values into all_groups list
+        all_groups.extend(group_dict)
+    df_result = pd.DataFrame.from_dict(all_groups)
+    df_result.to_csv("influxdata_2023-06-02T18_44_38Z_4.csv", index=False)
